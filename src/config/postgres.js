14882edf3a1418a02103a694aad0546a64016178
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-18 17:03:48 +0100

    Reduce application Postgres pool pressure

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -3,7 +3,10 @@ import env from "./env.js";
 
 const pool = new Pool({
   connectionString: env.POSTGRES_URL,
-  max: 10,
+  max: env.POSTGRES_POOL_MAX,
+  connectionTimeoutMillis: env.POSTGRES_CONNECTION_TIMEOUT_MS,
+  idleTimeoutMillis: 30000,
+  keepAlive: true,
 });
 
 export default pool;
