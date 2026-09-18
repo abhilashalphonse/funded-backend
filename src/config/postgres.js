0357f2caa4cde494307dac1cef08e413bef9c0d4
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-18 03:26:04 +0100

    Use POSTGRES_URL for direct Postgres pool

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -2,12 +2,8 @@ import { Pool } from "pg";
 import env from "./env.js";
 
 const pool = new Pool({
-  host: env.POSTGRES_HOST,
-  port: Number(env.POSTGRES_PORT || 5432),
-  user: env.POSTGRES_USER,
-  password: env.POSTGRES_PASSWORD,
-  database: env.POSTGRES_DATABASE,
-  max: 10
+  connectionString: env.POSTGRES_URL,
+  max: 10,
 });
 
 export default pool;
