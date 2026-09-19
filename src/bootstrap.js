b6f1f7e90fe6e3990c36aadd1c5fc0a72c0089d4
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-19 06:31:12 +0100

    Repair payment index during backend startup

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,9 +4,11 @@ dotenv.config();
 import { connectDatabase } from "./config/database.js";
 import boss from "./config/boss.js";
 import pool from "./config/postgres.js";
+import { ensurePaymentProviderIndex } from "./models/payment.model.js";
 
 export async function bootstrap() {
   await connectDatabase();
+  await ensurePaymentProviderIndex();
 
   await boss.start();
   await boss.createQueue("incoming-events");
