1950a1ad52eb2480f79b768ffa9e934d9aa892cf
Abhilash Alphonse <abhilashalphonse.dev@outlook.com>
2026-09-22 12:11:01 +0100

    Add UPI INR quote endpoint

--- src/apis/routes/payment.routes.js
+++ src/apis/routes/payment.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { optionalCustomer } from "../../auth/supabaseAuth.js";
-import { createCrypto, createUpi, ipn, rupayexCallback, status } from "../controllers/payment.controller.js";
+import { createCrypto, createUpi, ipn, rupayexCallback, status, upiQuote } from "../controllers/payment.controller.js";
 
 const router = Router();
 router.post("/crypto/create", optionalCustomer, createCrypto);
+router.post("/upi/quote", upiQuote);
 router.post("/upi/create", optionalCustomer, createUpi);
 router.post("/upi/rupayex/callback", rupayexCallback);
 router.get("/upi/rupayex/callback", rupayexCallback);
